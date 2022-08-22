ede8047bc683ad0a7fe8ea79a41f38c8ee471db9
Gleb Leus <lv.gleb44@gmail.com>
2022-08-22 13:22:37 +0300

    tree animation

--- game.js
+++ game.js
@@ -1,1 +1,45 @@
-console.log("uwu");
+(function () {
+    const car = document.querySelector('.car'),
+          gameButton = document.querySelector('.play-btn');
+    const trees = document.querySelectorAll('.tree');
+    const speed = 3;
+
+    let isPause = false;
+    let animationId = null;
+
+    const first = trees[0];
+
+    animationId = requestAnimationFrame(startGame);
+    function startGame () {
+        treesAnimation();
+
+        animationId = requestAnimationFrame(startGame);
+    }
+
+    function treesAnimation () { // анимация дерева
+        const newCoord = getCoordY(first) + speed; // получаем координату дерева и добавляем какое-то число
+        first.style.transform = `translateY(${newCoord}px)`; // перезаписываем новую координату
+    }
+
+    function getCoordY (element) { // функция для получения координаты Y элемента
+        const matrix = window.getComputedStyle(element).transform; // получаем координаты дерева
+        const array = matrix.split(','), // строчку с координатами делим на массив через запятую
+          lastElement = array[array.length - 1], // достаём последнюю координату
+          coordY = parseFloat(lastElement); // преобразование к числу
+
+        return coordY;
+    }
+
+    gameButton.addEventListener('click', () => { // переключение кнопки пауза\продолжить
+        isPause = !isPause;
+
+        if (isPause) {
+            cancelAnimationFrame(startGame);
+            gameButton.children[0].style.display = 'none';
+            gameButton.children[1].style.display = 'initial';
+        } else {
+            gameButton.children[0].style.display = 'initial';
+            gameButton.children[1].style.display = 'none';
+        }
+    })
+})();
